dc16589a49e4526c115c50e6c0538d0c45328e9e
Justin Kim <justin.joy.9to5@gmail.com>
2026-05-15 16:56:12 +0900

    fix: use installed wirelog for wyrelog head builds

--- Formula/wyrelog.rb
+++ Formula/wyrelog.rb
@@ -10,25 +10,24 @@ class Wyrelog < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
-  depends_on "wirelog"
-  depends_on "libchronoid"
-  depends_on "nanoarrow"
   depends_on "glib"
+  depends_on "libchronoid"
   depends_on "libsodium"
   depends_on "libsoup"
   depends_on "sqlite"
+  depends_on "wirelog"
 
   uses_from_macos "zlib"
 
-  on_macos do
-    resource "duckdb-osx-universal" do
+  resource "duckdb-osx-universal" do
+    on_macos do
       url "https://github.com/duckdb/duckdb/releases/download/v1.5.2/libduckdb-osx-universal.zip"
       sha256 "524f3537330a1b747556a0c98b62a46865a3f48c7ead2b2035c62f1ad3e5ca8b"
     end
   end
 
-  on_linux do
-    resource "duckdb-linux-amd64" do
+  resource "duckdb-linux-amd64" do
+    on_linux do
       url "https://github.com/duckdb/duckdb/releases/download/v1.5.2/libduckdb-linux-amd64.zip"
       sha256 "4711438f0fdb04f0441803409bec5430b763d4f2ac3482c1f97cfa6b5ecb4c15"
     end
@@ -42,6 +41,7 @@ class Wyrelog < Formula
     wirelog_lib = Formula["wirelog"].opt_lib
     ENV.append "LDFLAGS", "-Wl,-rpath,#{wirelog_lib}" if OS.mac?
     ENV.append "LDFLAGS", "-Wl,-rpath,#{wirelog_lib}" if OS.linux?
+    ENV.append "LDFLAGS", "-Wl,-rpath,#{Formula["libchronoid"].opt_lib}" if OS.linux?
 
     if OS.mac?
       duckdb_dir = buildpath/"subprojects/duckdb-prebuilt-osx-universal"
@@ -53,8 +53,41 @@ class Wyrelog < Formula
       cp buildpath/"subprojects/packagefiles/duckdb-prebuilt-linux/meson.build", duckdb_dir/"meson.build"
     end
 
+    inreplace "meson.build",
+      <<~MESON,
+        wirelog_proj = subproject(
+          'wirelog',
+          default_options : ['tests=false', 'documentation=false'],
+        )
+        wirelog_consumer_args = []
+        if host_machine.system() == 'windows'
+          wirelog_consumer_args += ['-DWIRELOG_BUILDING']
+        endif
+        wirelog_dep = declare_dependency(
+          link_with : wirelog_proj.get_variable('wirelog_lib'),
+          include_directories : [
+            wirelog_proj.get_variable('wirelog_inc'),
+            wirelog_proj.get_variable('wirelog_src_inc'),
+          ],
+          compile_args : wirelog_consumer_args,
+        )
+
+        libchronoid_proj = subproject(
+          'libchronoid',
+          default_options : ['tests=false', 'cli=false'],
+        )
+        libchronoid_dep = declare_dependency(
+          link_with : libchronoid_proj.get_variable('chronoid_lib'),
+          include_directories : libchronoid_proj.get_variable('inc'),
+        )
+      MESON
+      <<~MESON
+        wirelog_dep = dependency('wirelog', required : true)
+        libchronoid_dep = dependency('libchronoid', required : true)
+      MESON
+
     meson_args = std_meson_args.reject { |arg| arg.start_with?("--wrap-mode") }
-    system "meson", "setup", "build", "--wrap-mode=nofallback", *meson_args,
+    system "meson", "setup", "build", "--wrap-mode=nodownload", *meson_args,
            "-Denable_client=enabled",
            "-Denable_audit=enabled",
            "-Denable_fact_store=enabled",
@@ -64,6 +97,7 @@ class Wyrelog < Formula
            "-Dwyrelog_log_max_level=warn"
     system "meson", "compile", "-C", "build"
     system "meson", "install", "-C", "build"
+    rm share/"glib-2.0/schemas/gschemas.compiled" if (share/"glib-2.0/schemas/gschemas.compiled").exist?
 
     if OS.mac?
       lib.install buildpath/"subprojects/duckdb-prebuilt-osx-universal/libduckdb.dylib"
@@ -88,8 +122,8 @@ class Wyrelog < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/wyrelogd --version")
-    assert_match version.to_s, shell_output("#{bin}/wyctl --version")
-    assert_path_exists share/"wyrelog/access/manifest.ini"
+    assert_match stable.version.to_s, shell_output("#{bin}/wyrelogd --version")
+    assert_match stable.version.to_s, shell_output("#{bin}/wyctl --version")
+    assert_path_exists pkgshare/"access/manifest.ini"
   end
 end
